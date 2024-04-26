18fc4878fc4b823df4f94e76ff0d82cd26db93b1
isidor12 <isideriyomi@gmail.com>
2024-04-26 13:22:04 +0100

    corec

--- src/components/EditCopy/EditCopy.tsx
+++ src/components/EditCopy/EditCopy.tsx
@@ -25,11 +25,11 @@ const EditCopy: FC<EditCopyProps> = () => {
       console.log(color);
       setRows([""])
       console.log('lolavgit ~ kiloju');
-      
-      
+
+
       setTva(true)
       console.log("git");
-      
+
 
 
     }
@@ -165,35 +165,133 @@ const EditCopy: FC<EditCopyProps> = () => {
           <div className="warning">Veuillez utiliser le point au lieu de la virgule pour les nombres.</div>
 
         </div>
-        <div className="table">
-          <table className="table-auto">
-            <thead>
-              <tr>
-                <th className="border-double">reference</th>
-                <th className='border'>Quantite</th>
-                <th className='border'>Designation</th>
-                <th className='border'>unite</th>
-                <th className='border'>Prix unitaire</th>
-                <th className='border'>Montant HT </th>
-                {withTva && <th className='border'>TVA %</th>}
+        <div className="tables">
+          <table className="shadow-2xl font-[popins] border-2 table-auto w-11/12 border-collapse ">
+            <thead className='text-black'>
+              <tr style={{ backgroundColor: color }}>
+                <th className="py-3 border-1 border-black border-solid">reference</th>
+                <th className='py-3 border-2 border-solid  border-black' >Designation</th>
+                <th className='py-3 border-2 border-solid  border-black'>Quantite</th>
+                <th className='py-3 border-2 border-solid  border-black'>unite</th>
+                <th className='py-3 border-2 border-solid  border-black'>Prix unitaire</th>
+                <th className='py-3 border-2 border-solid  border-black'>Montant HT </th>
+                {withTva && <th className='py-3 border-solid border-2'>TVA %</th>}
 
 
-                <th></th>
+                <th className='bg-white'></th>
               </tr>
             </thead>
             <tbody>
               {rows.map((row) => (
                 <tr>
-                  <td className='border '> {row}</td>
-                  <td className='border'>manger</td>
-                  <td className='border'>manger</td>
-                  <td className='border'>manger</td>
-                  <td className='border'>manger</td>
-                  <td className='border'>manger</td>
-                  <td className='border'>manger</td>
+                  <td className='border-x-2 border-y-0 border-solid '> {row}</td>
+                  <td className=' border-x-2 border-y-0 border-solid'>
+                    <input type="text" name='designation' placeholder='entrer ici la designation' />
+                  </td>
+                  <td className=' border-x-2 border-y-0 border-solid'>
+                    <input type="number" className='w-[5rem]' name='Quantite' placeholder='0' />
+                  </td>
+                  <td className=' border-x-2 border-y-0 border-solid'>
+                    <select name="unite" id="">
+                      <option value=""></option>
+                      <option value="A">A</option>
+                      <option value="G">G</option>
+                      <option value="H">H</option>
+                      <option value="Img">Img</option>
+                      <option value="J">J</option>
+                      <option value="Kg">Kg</option>
+                      <option value="Km">Km</option>
+                      <option value="L">L</option>
+                      <option value="lot">lot</option>
+                      <option value="M">M</option>
+                      <option value="M2">M2</option>
+                      <option value="M3">M3</option>
+                      <option value="ML">ML</option>
+                      <option value="Min">Min</option>
+                      <option value="P">P</option>
+                      <option value="Pcs">Pcs</option>
+                      <option value="sec">sec</option>
+                      <option value="T">T</option>
+                    </select>
+                  </td>
+                  <td className=' p-1 border-x-2 border-y-0 border-solid'>
+                    <input type="number" className='w-[5rem]' name='priceUnit' placeholder='0.00' />
+                  </td>
+                  <td className=' border-x-2 border-y-0 border-solid'>
+                    <input type="text" className='w-[5rem]' name='priceUnit' defaultValue={"0.00"} />
+
+                  </td>
+                  {
+                    withTva && <td className=' border-x-2 border-y-0 border-solid'>
+                      <select name="Tva_taux">
+                        <option value="0.00" selected>0.00</option>
+                        <option value="1.00">1.00</option>
+                        <option value="2.00">2.00</option>
+                        <option value="2.10">2.10</option>
+                        <option value="2.40">2.40</option>
+                        <option value="2.50">2.50</option>
+                        <option value="3.00">3.00</option>
+                        <option value="3.80">3.80</option>
+                        <option value="4.00">4.00</option>
+                        <option value="4.50">4.50</option>
+                        <option value="4.80">4.80</option>
+                        <option value="5.00">5.00</option>
+                        <option value="5.50">5.50</option>
+                        <option value="6.00">6.00</option>
+                        <option value="6.50">6.50</option>
+                        <option value="7.00">7.00</option>
+                        <option value="7.60">7.60</option>
+                        <option value="8.00">8.00</option>
+                        <option value="8.50">8.50</option>
+                        <option value="8.75">8.75</option>
+                        <option value="9.00">9.00</option>
+                        <option value="10.00">10.00</option>
+                        <option value="10.50">10.50</option>
+                        <option value="11.00">11.00</option>
+                        <option value="11.50">11.50</option>
+                        <option value="12.00">12.00</option>
+                        <option value="12.50">12.50</option>
+                        <option value="13.00">13.00</option>
+                        <option value="13.50">13.50</option>
+                        <option value="14.00">14.00</option>
+                        <option value="15.00">15.00</option>
+                        <option value="16.00">16.00</option>
+                        <option value="17.00">17.00</option>
+                        <option value="18.00">18.00</option>
+                        <option value="19.00">19.00</option>
+                        <option value="19.25">19.25</option>
+                        <option value="19.60">19.60</option>
+                        <option value="20.00">20.00</option>
+                        <option value="20.60">20.60</option>
+                        <option value="21.00">21.00</option>
+                        <option value="22.00">22.00</option>
+                        <option value="23.00">23.00</option>
+                        <option value="24.00">24.00</option>
+                        <option value="25.00">25.00</option>
+                        <option value="26.00">26.00</option>
+                        <option value="27.00">27.00</option>
+                        <option value="28.00">28.00</option>
+                        <option value="29.00">29.00</option>
+                      </select>
+
+                    </td>
+
+                  }
 
                   <td>
-                    <button>Supprimer</button>
+                    <div className="action flex bg-gray-400">
+                      <div className="titles">
+                       <div className="title">
+                       <label htmlFor="addT" className=" text-blue-600 font-bold">T</label>
+                        <input type="checkbox" name="Tit" id="" />
+                       </div>
+                       <div className="sum">
+                       <label htmlFor="sumb" className=" text-orange-800 font-bold"> = </label>
+                        <input type="checkbox" name="summ" id="" />
+                       </div>
+                      </div>
+                    </div>
+                    
                   </td>
                 </tr>
               ))}
